ae9838381e31f2df9dbe5761ec7fbe140a27a253
Hannele Ruiz <justlemoncl@gmail.com>
2019-12-18 02:09:49 -0300

    Added command to kick everyone in the server

--- Bridge/Script.cs
+++ Bridge/Script.cs
@@ -1,4 +1,7 @@
 ﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
 
 namespace LambentLight.Bridge
 {
@@ -7,5 +10,27 @@ namespace LambentLight.Bridge
     /// </summary>
     public class Bridge : BaseScript
     {
+        public Bridge()
+        {
+            // Add the commands that we need
+            API.RegisterCommand("bridgekickall", new Action<int, List<object>, string>((s, a, r) => CommandKickAll(s, a, r)), false);
+        }
+
+        private void CommandKickAll(int source, List<object> args, string raw)
+        {
+            // If there is more than one argument, select the first one and use it as the kick message
+            // Otherwise, just say that the server is closing down
+            string message = args.Count >= 1 ? args[0].ToString() : "Server is shutting down.";
+
+            // Write a console message
+            Console.WriteLine($"Kicking all players with message '{message}'");
+
+            // Iterate over the players on the server
+            foreach (Player player in Players)
+            {
+                // And kick every single one of them
+                player.Drop(message);
+            }
+        }
     }
 }
